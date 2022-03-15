3431c6e7d0c8a103d26f95a22adb2649a0b0b5ee
Elena Kryuchkina <ekryuchkina@hse.ru>
2022-03-15 12:13:41 +0300

    Добавлен и заполнен отдельный файл f-n

---
+++ calculator/function.php
@@ -0,0 +1,27 @@
+<?php
+include __DIR__ . '/calculator.php';
+
+assert(calaculator(5,16,'plus'), 21);
+assert(calaculator(5,5,'multply'), 25);
+assert(calaculator(1,5,'divide'), -0.5);
+assert(calaculator(1,1,'minus'), 0);
+assert(calaculator(1,0,'minus'), 1);
+
+function calculator($number1, $number2, $operation)
+{
+    switch ($operation) {
+        case 'plus':
+            return $number1 + $number2;
+            break;
+        case 'minus':
+            return $number1 - $number2;
+            break;
+        case 'multply':
+            return $number1 * $number2;
+            break;
+        case 'divide':
+            return $number1 / $number2;
+            break;
+    }
+}
+return calculator($number1, $number2, $operation);
--- calculator/index.php
+++ calculator/index.php
@@ -1,6 +1,6 @@
 <?php
 
-include __DIR__ . '/calculator.php';
+include __DIR__ . '/function.php';
 
 ?>
 <!DOCTYPE HTML>
